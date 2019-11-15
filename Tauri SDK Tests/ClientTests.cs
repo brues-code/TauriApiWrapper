5d213175b3cee530b83ebe2908833789bbe6838d
Query <telmo.p.rodrigues@gmail.com>
2019-11-15 21:43:49 +0000

    Add tests

---
+++ Tauri SDK Tests/ClientTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tauri_SDK_Tests
+{
+    [TestClass]
+    public class ClientTests
+    {
+        [TestMethod]
+        public void TestClientConnection()
+        {
+        }
+    }
+}
