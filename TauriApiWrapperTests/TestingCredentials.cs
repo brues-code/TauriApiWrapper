9dac8644fe1049e35b9b0a3daa261ee3f3cbeeda
Telmo Rodrigues <660776+query-js@users.noreply.github.com>
2020-04-12 19:04:19 +0100

    CI fix

--- TauriApiWrapperTests/TestingCredentials.cs
+++ TauriApiWrapperTests/TestingCredentials.cs
@@ -6,9 +6,7 @@ namespace TauriApiWrapperTests
 {
     internal class TestingCredentials
     {
-#if DEBUG
         public const string ApiKey = "e26c8d840d406f0004badcf70f207a23";
         public const string Secret = "fcf1b92f000c99d61685f9629a4645218e746083";
-#endif
     }
 }
