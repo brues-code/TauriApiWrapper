94333c79b2c1d68e08aa7050ece54aeebe5a95df
Rdzio <jasjedrzejowski@gmail.com>
2020-04-25 17:07:51 +0200

    Added : Guild Raid, Raid Encounter Ranking, Guild Raid Encounter Ranking

---
+++ TauriApiWrapper/Objects/Requests/EncounterRankingRequest.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using TauriApiWrapper.Enums;
+
+namespace TauriApiWrapper.Objects.Requests
+{
+    internal sealed class EncounterRankingRequest : RaidListRequest
+    {
+        public EncounterRankingRequest(int encounter, int difficulty, Realm realm, int fromID = 0, int limit = 0) : base(realm, fromID: fromID, limit: limit)
+        {
+            Encounter = encounter;
+            Difficulty = difficulty;
+        }
+
+        [JsonProperty("encounter")]
+        public int Encounter { get; set; }
+
+        [JsonProperty("difficulty")]
+        public int Difficulty { get; set; }
+    }
+}
---
+++ TauriApiWrapper/Objects/Responses/Raid/RaidLogRanking.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using TauriApiWrapper.Converters;
+using TauriApiWrapper.Enums;
+using TauriApiWrapper.Objects.Responses.Guild;
+
+namespace TauriApiWrapper.Objects.Responses.Raid
+{
+    public sealed class RaidLogRanking : BaseApiResponse
+    {
+        [JsonProperty("rank")]
+        public int Rank { get; set; }
+
+        [JsonProperty("pos")]
+        public int Pos { get; set; }
+
+        [JsonProperty("log_id")]
+        public int LogId { get; set; }
+
+        [JsonProperty("map_id")]
+        public int MapId { get; set; }
+
+        [JsonProperty("mapentry")]
+        public Encounter Encounter { get; set; }
+
+        [JsonProperty("difficulty")]
+        public RaidDifficulty Difficulty { get; set; }
+
+        [JsonProperty("rid")]
+        public int RaidID { get; set; }
+
+        [JsonProperty("guildid")]
+        public int GuildID { get; set; }
+
+        [JsonProperty("guildrid")]
+        public int GuildRaidID { get; set; }
+
+        [JsonProperty("guilddata")]
+        public GuildData GuildData { get; set; }
+
+        [JsonProperty("killtime"), JsonConverter(typeof(UnixDateConverter))]
+        public DateTime KillTime { get; set; }
+
+        [JsonProperty("wipes")]
+        public int Wipes { get; set; }
+
+        [JsonProperty("deahts_total")]
+        public int DeathsTotal { get; set; }
+
+        [JsonProperty("fight_time"), JsonConverter(typeof(TimeSpanConverter))]
+        public TimeSpan FightTime { get; set; }
+
+        [JsonProperty("deaths_fight")]
+        public int Deaths { get; set; }
+
+        [JsonProperty("resurrects_fight")]
+        public int Resurrects { get; set; }
+
+        [JsonProperty("encounter_id")]
+        public int EncounterID { get; set; }
+
+        [JsonProperty("encounter_data")]
+        public Encounter EncounterData { get; set; }
+
+        [JsonProperty("member_count")]
+        public int MemberCount { get; set; }
+
+        [JsonProperty("item_count")]
+        public int ItemCount { get; set; }
+    }
+}
