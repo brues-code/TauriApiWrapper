0b1a9d0b30685440139fa4cd4b3fcbde987ceded
Query <telmo.p.rodrigues@gmail.com>
2020-07-03 17:38:40 +0100

    Added all the other expansions

--- TauriApiWrapper/Enums/Expansion.cs
+++ TauriApiWrapper/Enums/Expansion.cs
@@ -6,6 +6,10 @@
         BurningCrusade = 1,
         WrathOfTheLichKing = 2,
         Cataclysm = 3,
-        MistsOfPandaria = 4
+        MistsOfPandaria = 4,
+        WarlordsOfDraenor = 5,
+        Legion = 7,
+        BfA = 8,
+        Shadowlands = 9
     }
 }
