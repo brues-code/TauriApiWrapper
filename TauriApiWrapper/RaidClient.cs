d612f62f5ab68951351d15c4b37fd3ae3945d85b
Telmo Rodrigues <660776+query-wow@users.noreply.github.com>
2022-01-17 16:13:55 +0000

    Realm is now a required param

--- TauriApiWrapper/RaidClient.cs
+++ TauriApiWrapper/RaidClient.cs
@@ -28,37 +28,37 @@ namespace TauriApiWrapper
             return client.Communicate<RaidMaps>(param);
         }
 
-        public static ApiResponse<RaidLogsResponse> GetLatestRaids(TauriClient client, int? mapID = null, long? fromID = null, int limit = 0, Realm realm = Realm.Evermoon)
+        public static ApiResponse<RaidLogsResponse> GetLatestRaids(TauriClient client, Realm realm, int? mapID = null, long? fromID = null, int limit = 0)
         {
             ApiParams param = new ApiParams(Endpoints.LatestRaids, client.ApiSecret, new RaidListRequest(realm, mapID, fromID: fromID, limit: limit));
             return client.Communicate<RaidLogsResponse>(param);
         }
 
-        public static ApiResponse<RaidLog> GetRaidLogFromID(TauriClient client, int fromID, Realm realm = Realm.Evermoon)
+        public static ApiResponse<RaidLog> GetRaidLogFromID(TauriClient client, int fromID, Realm realm)
         {
             ApiParams param = new ApiParams(Endpoints.RaidLog, client.ApiSecret, new RaidLogRequest(fromID, realm));
             return client.Communicate<RaidLog>(param);
         }
 
-        public static ApiResponse<RaidLogsResponse> GetRaidLogFromPlayerName(TauriClient client, string name, long? fromID = null, int limit = 0, Realm realm = Realm.Evermoon)
+        public static ApiResponse<RaidLogsResponse> GetRaidLogFromPlayerName(TauriClient client, string name, Realm realm, long? fromID = null, int limit = 0)
         {
             ApiParams param = new ApiParams(Endpoints.RaidPlayer, client.ApiSecret, new PlayerRaidLogRequest(name, realm, fromID: fromID, limit: limit));
             return client.Communicate<RaidLogsResponse>(param);
         }
 
-        public static ApiResponse<RaidLogsResponse> GetRaidLogFromGuildName(TauriClient client, string name, long? fromID = null, int limit = 0, Realm realm = Realm.Evermoon)
+        public static ApiResponse<RaidLogsResponse> GetRaidLogFromGuildName(TauriClient client, string name, Realm realm, long? fromID = null, int limit = 0)
         {
             ApiParams param = new ApiParams(Endpoints.RaidGuild, client.ApiSecret, new GuildRaidLogRequest(name, realm, fromID: fromID, limit: limit));
             return client.Communicate<RaidLogsResponse>(param);
         }
 
-        public static ApiResponse<RaidEncounterRankingResponse> GetRaidEncounterRanking(TauriClient client, int encounter, int difficulty, long? fromID = null, int limit = 0, Realm realm = Realm.Evermoon)
+        public static ApiResponse<RaidEncounterRankingResponse> GetRaidEncounterRanking(TauriClient client, int encounter, int difficulty, Realm realm, long? fromID = null, int limit = 0)
         {
             ApiParams param = new ApiParams(Endpoints.RaidRankEcounter, client.ApiSecret, new EncounterRankingRequest(encounter, difficulty, realm, fromID: fromID, limit: limit));
             return client.Communicate<RaidEncounterRankingResponse>(param);
         }
 
-        public static ApiResponse<RaidEncounterRankingResponse> GetRaidGuildEncounterRanking(TauriClient client, int encounter, int difficulty, long? fromID = null, int limit = 0, Realm realm = Realm.Evermoon)
+        public static ApiResponse<RaidEncounterRankingResponse> GetRaidGuildEncounterRanking(TauriClient client, int encounter, int difficulty, Realm realm, long? fromID = null, int limit = 0)
         {
             ApiParams param = new ApiParams(Endpoints.RaidGuildRankEncounter, client.ApiSecret, new EncounterRankingRequest(encounter, difficulty, realm, fromID: fromID, limit: limit));
             return client.Communicate<RaidEncounterRankingResponse>(param);
@@ -68,43 +68,43 @@ namespace TauriApiWrapper
 
         #region Async
 
-        public static async Task<ApiResponse<RaidMaps>> GetRaidMapsAsync(TauriClient client, Realm realm = Realm.Evermoon)
+        public static async Task<ApiResponse<RaidMaps>> GetRaidMapsAsync(TauriClient client, Realm realm)
         {
             ApiParams param = new ApiParams(Endpoints.RaidMaps, client.ApiSecret, new RaidMapRequest(realm));
             return await client.CommunicateAsync<RaidMaps>(param);
         }
 
-        public static async Task<ApiResponse<RaidLogsResponse>> GetLatestRaidsAsync(TauriClient client, int? mapID = null, long? fromID = null, int limit = 50, Realm realm = Realm.Evermoon)
+        public static async Task<ApiResponse<RaidLogsResponse>> GetLatestRaidsAsync(TauriClient client, Realm realm,  int? mapID = null, long? fromID = null, int limit = 50)
         {
             ApiParams param = new ApiParams(Endpoints.LatestRaids, client.ApiSecret, new RaidListRequest(realm, mapID, fromID: fromID, limit: limit));
             return await client.CommunicateAsync<RaidLogsResponse>(param);
         }
 
-        public static async Task<ApiResponse<RaidLog>> GetRaidLogFromIDAsync(TauriClient client, int fromID, Realm realm = Realm.Evermoon)
+        public static async Task<ApiResponse<RaidLog>> GetRaidLogFromIDAsync(TauriClient client, int fromID, Realm realm)
         {
             ApiParams param = new ApiParams(Endpoints.RaidLog, client.ApiSecret, new RaidLogRequest(fromID, realm));
             return await client.CommunicateAsync<RaidLog>(param);
         }
 
-        public static async Task<ApiResponse<RaidLog>> GetRaidLogFromPlayerNameAsync(TauriClient client, string name, long? fromID = null, int limit = 0, Realm realm = Realm.Evermoon)
+        public static async Task<ApiResponse<RaidLog>> GetRaidLogFromPlayerNameAsync(TauriClient client, string name, Realm realm, long? fromID = null, int limit = 0)
         {
             ApiParams param = new ApiParams(Endpoints.RaidPlayer, client.ApiSecret, new PlayerRaidLogRequest(name, realm, fromID: fromID, limit: limit));
             return await client.CommunicateAsync<RaidLog>(param);
         }
 
-        public static async Task<ApiResponse<RaidLogsResponse>> GetRaidLogFromGuildNameAsync(TauriClient client, string name, long? fromID = null, int limit = 0, Realm realm = Realm.Evermoon)
+        public static async Task<ApiResponse<RaidLogsResponse>> GetRaidLogFromGuildNameAsync(TauriClient client, string name, Realm realm, long? fromID = null, int limit = 0)
         {
             ApiParams param = new ApiParams(Endpoints.RaidGuild, client.ApiSecret, new GuildRaidLogRequest(name, realm, fromID: fromID, limit: limit));
             return await client.CommunicateAsync<RaidLogsResponse>(param);
         }
 
-        public static async Task<ApiResponse<RaidEncounterRankingResponse>> GetRaidEncounterRankingAsync(TauriClient client, int encounter, int difficulty, long? fromID = null, int limit = 0, Realm realm = Realm.Evermoon)
+        public static async Task<ApiResponse<RaidEncounterRankingResponse>> GetRaidEncounterRankingAsync(TauriClient client, int encounter, int difficulty, Realm realm, long? fromID = null, int limit = 0)
         {
             ApiParams param = new ApiParams(Endpoints.RaidRankEcounter, client.ApiSecret, new EncounterRankingRequest(encounter, difficulty, realm, fromID: fromID, limit: limit));
             return await client.CommunicateAsync<RaidEncounterRankingResponse>(param);
         }
 
-        public static async Task<ApiResponse<RaidEncounterRankingResponse>> GetRaidGuildEncounterRankingAsync(TauriClient client, int encounter, int difficulty, long? fromID = null, int limit = 0, Realm realm = Realm.Evermoon)
+        public static async Task<ApiResponse<RaidEncounterRankingResponse>> GetRaidGuildEncounterRankingAsync(TauriClient client, int encounter, int difficulty, Realm realm, long? fromID = null, int limit = 0)
         {
             ApiParams param = new ApiParams(Endpoints.RaidGuildRankEncounter, client.ApiSecret, new EncounterRankingRequest(encounter, difficulty, realm, fromID: fromID, limit: limit));
             return await client.CommunicateAsync<RaidEncounterRankingResponse>(param);
--- TauriApiWrapperTests/GuildClientTester.cs
+++ TauriApiWrapperTests/GuildClientTester.cs
@@ -15,7 +15,7 @@ namespace TauriApiWrapperTests
         {
             TauriClient client = new TauriClient(_credentials.ApiKey, _credentials.ApiSecret, false);
             {
-                ApiResponse<GuildRoster> returnData = GuildClient.GetGuildRoster(client, "Muzykanci z Gruzji");
+                ApiResponse<GuildRoster> returnData = GuildClient.GetGuildRoster(client, "Muzykanci z Gruzji", TauriApiWrapper.Enums.Realm.Evermoon);
                 Assert.IsTrue(returnData.IsSuccess);
                 Assert.IsNotNull(returnData.Response);
                 Assert.IsTrue(returnData.Response.GuildList.Count > 0);
@@ -27,7 +27,7 @@ namespace TauriApiWrapperTests
         {
             TauriClient client = new TauriClient(_credentials.ApiKey, _credentials.ApiSecret, false);
             {
-                ApiResponse<GuildStats> returnData = GuildClient.GetGuildStats(client, "Muzykanci z Gruzji");
+                ApiResponse<GuildStats> returnData = GuildClient.GetGuildStats(client, "Muzykanci z Gruzji", TauriApiWrapper.Enums.Realm.Evermoon);
                 Assert.IsTrue(returnData.IsSuccess);
                 Assert.IsNotNull(returnData.Response);
                 Assert.IsTrue(returnData.Response.GuildList.Count > 0);
@@ -39,7 +39,7 @@ namespace TauriApiWrapperTests
         {
             TauriClient client = new TauriClient(_credentials.ApiKey, _credentials.ApiSecret, false);
             {
-                ApiResponse<GuildBankContent> returnData = GuildClient.GetGuildBankContents(client, "Muzykanci z Gruzji");
+                ApiResponse<GuildBankContent> returnData = GuildClient.GetGuildBankContents(client, "Muzykanci z Gruzji", TauriApiWrapper.Enums.Realm.Evermoon);
                 Assert.IsTrue(returnData.IsSuccess);
                 Assert.IsNotNull(returnData.Response);
                 Assert.IsTrue(returnData.Response.GuildBankItemList.Count > 0);
@@ -51,7 +51,7 @@ namespace TauriApiWrapperTests
         {
             TauriClient client = new TauriClient(_credentials.ApiKey, _credentials.ApiSecret, false);
             {
-                ApiResponse<GuildBankLog> returnData = GuildClient.GetGuildBankLogs(client, "Muzykanci z Gruzji");
+                ApiResponse<GuildBankLog> returnData = GuildClient.GetGuildBankLogs(client, "Muzykanci z Gruzji", TauriApiWrapper.Enums.Realm.Evermoon);
                 Assert.IsTrue(returnData.IsSuccess);
                 Assert.IsNotNull(returnData.Response);
                 Assert.IsTrue(returnData.Response.GuildBankItemList.Count > 0);
